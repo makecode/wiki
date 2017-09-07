9e9ee204de2bb66911bb3cf1a37ba44116ff6ed9
Tony Chupak <tony.chupak@gmail.com>
2017-09-07 23:56:30 +0300

    fix bugs

--- build/js/app.js
+++ build/js/app.js
@@ -209,4 +209,14 @@ $(document).ready(function() {
     $('body,html').animate({scrollTop: top - margin}, 1000);
   });
 
+	var extension = $('.file-ext');
+
+	extension.map(function(el) {
+		if (el.length > 8) {
+			el.addClass('is-shorted');
+		}
+	})
+
+
 });
+
--- build/js/common.js
+++ build/js/common.js
@@ -18,4 +18,13 @@ $(document).ready(function() {
     $('body,html').animate({scrollTop: top - margin}, 1000);
   });
 
+	var extension = $('.file-ext');
+
+	extension.map(function(el) {
+		if (el.length > 8) {
+			el.addClass('is-shorted');
+		}
+	})
+
+
 });
